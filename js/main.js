93bbde044cfee1533248f1183404e691a8f32d1e
Mary Kasparian <kasparian.mary@gmail.com>
2022-04-14 15:11:04 -0500

    Add showSections function to show the section when the link is clicked on

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-// ====== SHOW NAVBAR ========
+const allSections = document.querySelectorAll("section");
+// list of all the nav links (a elements)
+const linkItem = document.querySelectorAll('.nav__link');
+
+// ================== SHOW SECTIONS FUNCTION========================
+const showSection = function(element) {
+  // iterate over all the sections and remove active class
+  for(let i = 0; i < allSections.length; i++) {
+    allSections[i].classList.remove("active")
+  }
+  // get the link attribute - returns ['', 'about']: select second item from array and store in target variable
+  const target = element.getAttribute("href").split("#")[1]
+  console.log(target)
+  // add the active class to the section with same id as target
+  document.querySelector('#' + target).classList.add("active")
+}
+
+// ================== SHOW NAVBAR ========================
 const showNavbar = (toggleId, navId, bodyId, headerId) => {
   const toggle = document.getElementById(toggleId);
   const nav = document.getElementById(navId);
@@ -26,19 +43,18 @@ const showNavbar = (toggleId, navId, bodyId, headerId) => {
 
 showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
 
-// ======= LINK ACTIVE ========
-// list of all the nav links (a elements)
-const linkColor = document.querySelectorAll('.nav__link');
-
-linkColor.forEach(function(link) {
+// ====================== ACTIVE LINK & SHOW SECTION========================
+linkItem.forEach(function(link) {
   // add an event listener to each link (<a> element)
   link.addEventListener("click", function() {
     // remove active class from other links
-    for(let i = 0; i < linkColor.length; i++) {
-      linkColor[i].classList.remove("active")
+    for(let i = 0; i < linkItem.length; i++) {
+      linkItem[i].classList.remove("active")
     }
     // add active to link clicked on
     this.classList.add("active");
+    // pass the clicked on link to showSection() function, to activate the right section
+    showSection(this)
   })
 })
 
