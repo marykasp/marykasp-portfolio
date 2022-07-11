79a43b362d9a893736343957ade8f687a1c7d5bc
Mary Kasparian <kasparian.mary@gmail.com>
2022-07-11 12:24:45 -0500

    Add toast notification to indicate theme change

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,10 +1,34 @@
 // ================= Toggle Style Switcher ===========================
 const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
 const styleSwitcher = document.querySelector(".style-switcher");
+const colors = document.querySelectorAll(".colors span");
+const toast = document.querySelector(".toast");
+const progress = document.querySelector(".progress");
+const closeIcon = document.querySelector(".close");
+
+console.log(progress);
 
 // show style theme switcher on click
 styleSwitcherToggler.addEventListener("click", function () {
   styleSwitcher.classList.toggle("open");
+});
+
+// iterate over the colors and add a click event for each to trigger the toast notification
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    toast.classList.add("active");
+    progress.classList.add("play");
+
+    setTimeout(() => {
+      toast.classList.remove("active");
+      progress.classList.remove("play");
+    }, 5000);
+  });
+});
+
+// close toast notification when click on close icon
+closeIcon.addEventListener("click", () => {
+  toast.classList.remove("active");
 });
 
 // hide style theme switcher on scroll
